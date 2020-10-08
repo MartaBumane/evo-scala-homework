bb6621a9257f638413b6c06f5fd2cb3363852a9f
Marta Bumane <mbumane2@evolutiongaming.com>
2020-10-08 12:04:46 +0300

    Fix sortConsideringEqualValues again

--- src/main/scala/datastructures/DataStructures.scala
+++ src/main/scala/datastructures/DataStructures.scala
@@ -3,12 +3,15 @@ package datastructures
 object DataStructures {
     def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = {
         val groupedByValue = map.groupBy { case (_, value) => value }
-        val mapSorted = groupedByValue.toList.map {
-            case (key, values) => (values.keySet, key)
-        }
-        val resultMap = mapSorted.sortBy {
-            case (_, value) => value
-        }
-        resultMap;
+        val mapSorted = groupedByValue
+            .toList
+            .map {
+                case (key, values) => (values.keySet, key)
+            }
+            .sortBy {
+                case (_, value) => value
+            }
+            
+        mapSorted;
     }
 }
